8edc5586c04bc19b7d0abb266fcfe97d181ba6d5
Atharv Shivarkar <atharvshivarkar@Atharvs-MacBook-Pro.local>
2023-11-11 12:46:10 +0530

    Added type declaration for variables

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Atharvs Conference"
-	const tickets = 50
-	var remainingTickets = 50
+	conferenceName := "Atharvs Conference"
+	const tickets int = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("Total tickets: %v Remaining tickets: %v\n", tickets, remainingTickets)
+
+	var userName string
+	var userTickets uint
+
+	userName="Golang"
+	userTickets=50
+
+	fmt.Printf("User %v booked %v tickes", userName, userTickets)
 }
